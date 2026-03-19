f0c34f6b4cb6c1783ab904c8d7eeefa6c022f513
ashmanoy <ashleysumanoy@gmail.com>
2026-03-19 20:32:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+document.querySelector('.joyous').addEventListener('click', () => {
+    new Audio('sfx/yippee-tbh.mp3').play();
+});
+
+document.querySelector('.cringed').addEventListener('click', () => {
+    new Audio('sfx/oh-no-cringe.mp3').play();
+});
+
+document.querySelector('.awkwardness').addEventListener('click', () => {
+    new Audio('sfx/huh.mp3').play();
+});
+
+document.querySelector('.pain').addEventListener('click', () => {
+    new Audio('sfx/ack.mp3').play();
+});
+
+document.querySelector('.pain-but-it-disappeared').addEventListener('click', () => {
+    new Audio('sfx/victory.mp3').play();
+});
+
+document.querySelector('.shocked').addEventListener('click', () => {
+    new Audio('sfx/shocked.mp3').play();
+});
+
+document.querySelector('.true-happiness').addEventListener('click', () => {
+    new Audio('sfx/happy.mp3').play();
+});
+
+document.querySelector('.william-final-form').addEventListener('click', () => {
+    new Audio('sfx/freddy-fazbear.mp3').play();
+});
